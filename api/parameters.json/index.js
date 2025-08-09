7f66b738a250ea5a7eac494130ea89552e323694
hamza-oun <63359223+hamza-oun@users.noreply.github.com>
2025-08-10 00:26:29 +0200

    Update index.js

--- api/parameters.json/index.js
+++ api/parameters.json/index.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const apiKey = process.env.API_KEY || process.env.FORESHADOW_API_KEY;
   const origin = process.env.PROXY_ORIGIN || 'https://mozn-proxy.vercel.app';
 
-  if (!apiKey) return res.status(403).json({ error: "Missing API key in env" });
+  if (!apiKey) return res.status(403).json({ error: "An API Key is required to make this request" });
 
   try {
     const r = await fetch("https://api.4shadow.io/v1/parameters", {
